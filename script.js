c6d4957ea2da6c97e3a2fa3b6f81b04aeed08f17
ggriffi <ggriffi@gmail.com>
2025-12-03 10:01:40 -0600

    updates

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.getElementById('mobileMenu').addEventListener('click', () => {
-    const nav = document.getElementById('navLinks');
-    nav.classList.toggle('active');
+    document.getElementById('navLinks').classList.toggle('active');
 });
 document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', () => {
